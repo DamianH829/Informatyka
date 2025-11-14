8d289cd42c97ee69029681306eee9ffeb51e08c3
DamianH829 <dharasimuk2@gmail.com>
2025-11-14 13:04:31 +0100

    Komentarz

---
+++ 1. Pierwszy program w C++/Zadanie 2; Zmienna v2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a = 5;
+    int b = 3;
+    cout << "Liczba A: " << a << endl;
+	cout << "Liczba B: " << b << endl;
+	cout << "Suma: " << a + b << endl;
+	cout << "Roznica: " << a - b << endl;
+	cout << "Iloczyn: " << a * b;
+    return 0;
+}
---
+++ 1. Pierwszy program w C++/Zadanie 2; Zmienna.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int liczba_a;
+    int liczba_b;
+    
+    cin >> liczba_a;
+	cin >> liczba_b;
+	
+	cout << liczba_a + liczba_b;
+	cout << "   ";
+	cout << liczba_a - liczba_b;
+	cout << "   ";
+	cout << liczba_a * liczba_b;
+}
